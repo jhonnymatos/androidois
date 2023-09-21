6b1a3e6b023144f0a0b8c5d19e23717a35303bec
jhonnymatos <joaovictor1218@outlook.com.br>
2023-09-20 21:23:23 -0300

    projetodois

---
+++ app/src/main/java/com/example/androidois/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.androidois
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import com.example.androidois.ui.theme.AndroidoisTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidoisTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    var vlrDoTextField  by remember {
+                        mutableStateOf("")
+                    }
+                    Column() {
+                        TextField(value = vlrDoTextField, onValueChange = {
+                            vlrDoTextField = it;
+                            Log.i( "###",vlrDoTextField)
+                        })
+                    }
+                }
+            }
+        }
+    }
+}
